b8d4b441c914879323f37889d306a228ee3226d3
vinod.gandamalla@technoidentity.com <vinod.gandamalla@technoidentity.com>
2017-08-30 11:25:44 +0530

    change services using immutable js

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { List } from 'immutable';
 
 @Injectable()
 export class TodoService {
-  todos: any[] = [
+  todos: List<{ label: string; id: number; complete: boolean }> = List([
     {
       label: 'Eat pizza',
       id: 0,
@@ -23,61 +24,62 @@ export class TodoService {
       id: 3,
       complete: true
     }
-  ];
+  ]);
 
   getTodos() {
-    return [
-      {
-        label: 'Eat pizza',
-        id: 0,
-        complete: true
-      },
-      {
-        label: 'Do some coding',
-        id: 1,
-        complete: true
-      },
-      {
-        label: 'Sleep',
-        id: 2,
-        complete: false
-      },
-      {
-        label: 'Print tickets',
-        id: 3,
-        complete: true
-      }
-    ];
+    return this.todos;
   }
 
   addTodo({ label }) {
-    this.todos = [{ label, id: this.todos.length + 1 }, ...this.todos];
+    this.todos = this.todos.push({
+      label: label,
+      id: this.todos.count() + 1,
+      complete: false
+    });
+    // this.todos = [{ label, id: this.todos.length + 1 }, ...this.todos];
   }
 
   completeTodo({ todo }) {
-    // console.log(todo);
-    this.todos = this.todos.map(
-      item =>
-        item.id === todo.id
-          ? Object.assign({}, item, { complete: !item.complete })
-          : item
-    );
+    this.todos.setIn([this.todos.findIndex(item => item.id === todo.id)], {
+      id: todo.id,
+      label: todo.label,
+      complete: todo.complete
+    });
+    // this.todos = this.todos.map(
+    // item =>
+    // item.id === todo.id
+    // ? Object.assign({}, item, { complete: !item.complete })
+    // : item
+    // );
   }
 
   removeTodo({ todo }) {
-    this.todos = this.todos.filter(({ id }) => id !== todo.id);
+    this.todos = this.todos.deleteIn([
+      this.todos.findIndex(item => item === todo)
+    ]);
+    // this.todos = this.todos.filter(({ id }) => id !== todo.id);
   }
 
   getById(id) {
     return this.todos.find(todo => todo.id === id);
+    // return this.todos.find(todo => todo.id === id);
   }
 
   saveTodo({ todo }) {
-    this.todos = this.todos.map(
-      item =>
-        item.id === parseInt(todo.id, 0)
-          ? Object.assign({}, item, { label: todo.label })
-          : item
+    this.todos = this.todos.setIn(
+      [this.todos.findIndex(item => item.id === todo.id)],
+      {
+        id: todo.id,
+        label: todo.label,
+        complete: todo.complete
+      }
     );
+    //   this.todos = this.todos.map(
+    //     item =>
+    //       item.id === parseInt(todo.id, 0)
+    //         ? Object.assign({}, item, { label: todo.label })
+    //         : item
+    //   );
+    // }
   }
 }
