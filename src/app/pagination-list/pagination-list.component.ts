54aac5885c19a0bf16a43abecc605ee576c00611
vinodgandamalla <vinod.gandamalla@technoidentity.com>
2017-09-04 14:41:05 +0530

    search and pagination list

--- src/app/pagination-list/pagination-list.component.ts
+++ src/app/pagination-list/pagination-list.component.ts
@@ -8,7 +8,8 @@ import { List } from 'immutable';
 })
 export class PaginationListComponent {
   store = this.todoService.getTodos();
-  todos = this.store.slice(0, 3);
+  mytodos = this.store;
+  todos = this.mytodos.slice(0, 3);
   temp;
   first = 0;
   last = 3;
@@ -17,34 +18,36 @@ export class PaginationListComponent {
     this.first = this.first - 3;
     this.last = this.last - 3;
 
-    this.temp = this.store.slice(this.first, this.last);
+    this.temp = this.mytodos.slice(this.first, this.last);
     this.todos = this.temp;
   }
   next() {
     this.first = this.first + 3;
     this.last = this.last + 3;
-    this.temp = this.store.slice(this.first, this.last);
+    this.temp = this.mytodos.slice(this.first, this.last);
     this.todos = this.temp;
   }
 
   delete({ todo }) {
     this.todoService.removeTodo({ todo });
-    this.store = this.todoService.getTodos();
-    this.todos = this.store.slice(this.first, this.last);
+    this.mytodos = this.todoService.getTodos();
+    this.todos = this.mytodos.slice(this.first, this.last);
     if (this.todos === List([]) && this.first > 0 && this.last > 3) {
       this.first = this.first - 3;
       this.last = this.last - 3;
-      this.todos = this.store.slice(this.first, this.last);
-      this.count = this.store.count();
+      this.todos = this.mytodos.slice(this.first, this.last);
+      this.count = this.mytodos.count();
     } else {
-      this.todos = this.store.slice(this.first, this.last);
-      this.count = this.store.count();
+      this.todos = this.mytodos.slice(this.first, this.last);
+      this.count = this.mytodos.count();
     }
   }
   search({ input }) {
-    this.todos = this.todoService.searchTodo({ input });
-
-    this.count = this.todos.count();
+    this.mytodos = this.todoService.searchTodo({ input });
+    this.todos = this.mytodos.slice(0, 3);
+    this.first = 0;
+    this.last = 3;
+    this.count = this.mytodos.count();
   }
   constructor(private todoService: TodoService) {}
 }
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -2,8 +2,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
-  template: `<div> <h1> {{this.first}},{{this.last}},{{this.count}}</h1> <input *ngIf="this.first> 0 && this.last > 3" type= "button" value= "prev" (click)="prev.emit() "/> <input *ngIf="this.last <  this.count" type= "button" value= "next" (click)="next.emit()" />
+  template: `<div> <input *ngIf="this.first> 0 && this.last > 3" type= "button" value= "prev" (click)="prev.emit() "/> <input *ngIf="this.last <  this.count" type= "button" value= "next" (click)="next.emit()" />
 </div>`
+  //<h1> {{this.first}},{{this.last}},{{this.count}}</h1>
   // templateUrl: './pagination.component.html',
   // styleUrls: ['./pagination.component.css']
 })
